dfbfdf8545b7ee26e6dcba368e6915004c14225e
Quannisme <121941593+Quannisme@users.noreply.github.com>
2023-08-13 10:24:39 +0700

    2

--- ss02/Caculator/src/main/java/code/vn/controller/CaculatorController.java
+++ ss02/Caculator/src/main/java/code/vn/controller/CaculatorController.java
@@ -9,33 +9,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CaculatorController {
     @GetMapping("/caculator/list")
-    public String showCaculator(){
+    public String showCaculator() {
         return "caculator/list";
     }
-    @GetMapping("/submit")
-    public String doo(@RequestParam("first") int f, @RequestParam("second") int s,@RequestParam("submit") String sub , Model model)
-    {
-        int cong=f+s;
-        int tru=f-s;
-        int nhan=f*s;
-        int chia=f/s;
-        int result;
-        switch (sub)
-        {
-            case "cong":
-                result=cong;
-                break;
-            case "tru":
-                result=tru;
-                break;
-            case "nhan":
-                result=nhan;
-                break;
-            default:
-                result=chia;
 
+    @GetMapping("/submit")
+    public String doCaculator(@RequestParam("first") int f, @RequestParam("second") int s, @RequestParam("submit") String sub, Model model) {
+
+        int result;
+        if(s==0)
+        {
+            result=Integer.MIN_VALUE;
+            model.addAttribute("errorMessage", "Cannot divide by zero");
+        }else {
+            int cong = f + s;
+            int tru = f - s;
+            int nhan = f * s;
+            int chia = f / s;
+            switch (sub) {
+                case "cong":
+                    result = cong;
+                    break;
+                case "tru":
+                    result = tru;
+                    break;
+                case "nhan":
+                    result = nhan;
+                    break;
+                default:
+                    result = chia;
+
+            }
         }
-        model.addAttribute("result",result);
-        return "caculator/list";
+            model.addAttribute("result", result);
+            return "caculator/list";
     }
 }
